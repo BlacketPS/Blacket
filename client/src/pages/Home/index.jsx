8b2e37ee61156f0f52268073bfe4190664db429b
monkxy <monkxy@blacket.org>
2023-09-14 21:32:11 -0500

    add home page

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,5 +1,52 @@
+import { isMobile } from "react-device-detect";
+import { Link } from "react-router-dom";
+import styles from "./home.module.css";
+
 export default function Home() {
     return (
-        <h1>ben</h1>
+        <>
+            <div className={styles.headerContainer}>
+                <img src="/content/homeBlooks.png" alt="Blooks" className={styles.headerImage} draggable="false" />
+
+                <div class={styles.background}>
+                    <div class={styles.blooksBackground} style={{ backgroundImage: "url(\"/content/background.png\")" }}></div>
+                </div>
+
+                <div className={isMobile ? styles.mHeaderSide : styles.headerSide}></div>
+
+                <div className={styles.topHeaderContainer}>
+                    <div className={styles.logoText}>
+                        name
+                    </div>
+                </div>
+
+                <div className={isMobile ? styles.mWelcomeContainer : styles.welcomeContainer}>
+                    <div className={isMobile ? styles.mWelcomeText : styles.welcomeText}>
+                        welcome
+                    </div>
+                    <div className={isMobile ? styles.mWelcomeDesc : styles.welcomeDesc}>
+                        description
+                    </div>
+
+                    <Link className={isMobile ? styles.mWelcomeButton : styles.welcomeButton} to="/register">
+                        Get Started
+                    </Link>
+
+                    <div className={isMobile ? styles.mPronounceButton : styles.pronounceButton} onClick={() => new Audio("/pronounce.mp3").play()}>
+                        <i className="fas fa-volume-up" />
+                        &nbsp;
+                        Pronunciation ("")
+                    </div>
+                </div>
+            </div>
+            <div class={styles.topButtonContainer}>
+                <Link className={`${styles.topButton} ${styles.loginButton}`} to="/login">
+                    Login
+                </Link>
+                <Link class={`${styles.topButton} ${styles.registerButton}`} to="/register">
+                    Register
+                </Link>
+            </div>
+        </>
     )
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,47 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react-swc"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    modules: {
+      scopeBehaviour: "local",
+      localsConvention: "camelCaseOnly",
+      generateScopedName: "[local]_[hash:base64:5]",
+    },
+  },
+  build: {
+    inlineDynamicImports: true,
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("node_modules")) return "vendor";
+          else return "main";
+        },
+        chunkFileNames: "[name].[hash].js",
+        entryFileNames: "[name].[hash].js",
+        assetFileNames: "[name].[hash].[ext]",
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+    manifest: true,
+    minify: "terser",
+    // outDir: "/var/www/monkxy/site/public",
+    terserOptions: {
+      format: {
+        comments: false,
+      },
+      compress: {
+        sequences: true,
+        booleans: true,
+        loops: true,
+        toplevel: true,
+        unsafe: true,
+        drop_console: true,
+        unsafe_comps: true,
+        passes: 2
+      },
+      module: true,
+    },
+  },
+});
