ab3a8ec85fe4d32ae9d905ff4418956697000a9a
monkxy <monkxy@blacket.org>
2023-09-14 20:51:06 -0500

    blooket

---
+++ client/src/main.jsx
@@ -0,0 +1,15 @@
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import axios from "axios";
+import pages from "./pages";
+
+const router = createBrowserRouter([
+  { path: "/", element: <pages.Home /> },
+]);
+
+axios.interceptors.request.use((config) => {
+  if (config.url.startsWith("/api")) config.url = ``;
+  return config;
+});
+
+ReactDOM.createRoot(document.getElementById("app")).render(<RouterProvider router={router} />);  
---
+++ index.js
@@ -0,0 +1,5 @@
+import { Elysia } from "elysia";
+
+new Elysia().listen(3000);
+
+console.log("Server running on port 3000");
