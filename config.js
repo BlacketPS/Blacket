50576ff0f0e2b3079ce99d18635371a6e053cfc1
monkxy <monkxy@blacket.org>
2023-09-15 20:55:30 -0500

    convert backend to ts
    Co-Authored-By: Xotic <twilightgeometrydash@gmail.com>

--- config.js
+++
@@ -1,4 +0,0 @@
-export default {
-    port: 3000,
-    verbose: true
-}
---
+++ index.ts
@@ -0,0 +1,15 @@
+import("dotenv/config");
+import config from "./config.json";
+import { Elysia } from "elysia";
+import { Sequelize } from "sequelize";
+import walker from "folder-walker";
+
+global.config = config;
+global.database = await new Sequelize(process.env.DATABASE_NAME ? process.env.DATABASE_NAME : "blacket", process.env.DATABASE_USER ? process.env.DATABASE_USER : "root", process.env.DATABASE_PASSWORD ? process.env.DATABASE_PASSWORD : "", {
+    host: process.env.DATABASE_HOST ? process.env.DATABASE_HOST : "localhost",
+    dialect: "mysql",
+    logging: config.verbose ? console.log : false
+});
+
+const endpoints = await walker("./endpoints");
+endpoints.on("data", (data) => data.basename.endsWith(".js") && import(data.path).then((endpoint) => typeof endpoint.default === "function" && endpoint.default()));
