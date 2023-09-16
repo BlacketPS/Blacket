7c0c0f75a71c2a68f3a6bc2434ccb08c5cd3d352
monkxy <monkxy@blacket.org>
2023-09-15 20:44:38 -0500

    change eslint to ts

--- client/.eslintrc.ts
+++ client/.eslintrc.ts
@@ -7,7 +7,7 @@ export default {
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  ignorePatterns: ["dist", ".eslintrc.ts"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
   plugins: ["react-refresh"],
